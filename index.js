b5f9c50705ccf98e3d79220e8cb89dcd64f03499
Niravpatel129 <niravpatelp129@gmail.com>
2020-01-18 03:09:14 -0500

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+var app = require("express")();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+
+const router = require("./router");
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log("Server started on port", PORT);
+});
+
+app.use(router);
+
+io.on("connection", socket => {
+  socket.on("drawingData", ({ data, room }) => {
+    socket.broadcast.emit("updateData", data);
+  });
+});
