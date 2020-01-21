a49abd074d93f7ffae45e1774c399b8600c21f6b
Niravpatel129 <niravpatelp129@gmail.com>
2020-01-21 03:13:56 -0500

    update

--- utils/roomData.js
+++ utils/roomData.js
@@ -86,11 +86,7 @@ class roomData {
 
     if (!find) {
       this.users.push({ user, points: 0, id: user.googleUserInfo.googleId });
-      this.addMessage(
-        "[Admin]",
-        user.googleUserInfo.name,
-        "Has joined the room"
-      );
+
       return true;
     } else {
       return false;
